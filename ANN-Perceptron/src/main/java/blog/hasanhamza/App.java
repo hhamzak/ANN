de1041af85dcf719741488d2aa5c4632e169de82
HAMZA-PC\HAMZA <hasanhamzakaldirim@gmail.com>
2024-01-19 23:04:03 +0300

    update threshold to -1

--- ANN-Perceptron/src/main/java/blog/hasanhamza/App.java
+++ ANN-Perceptron/src/main/java/blog/hasanhamza/App.java
@@ -5,7 +5,7 @@ public class App
     public static void main( String[] args )
     {
         double[] weights =  new double[] {1,2};
-        double threshold = 1;
+        double threshold = -1;
         double learningRate = 0.5;
 
         double[][] learningInputSet = {{1,0},{0,1}};
